7a63f6eda9b355b5a784d1aa2e2558c872939057
Bernard Amankwah <bernardamankwah@hotmail.com>
2023-02-01 15:56:28 +0100

    js basics

---
+++ index.js
@@ -0,0 +1,182 @@
+//VARIABLES
+
+// let x = 9;
+// x = 11;
+
+// const y = 10;
+
+// console.log(x, y);
+
+// let _personName
+
+//DATA TYPES (STRING, NUMBER, BOOLEAN, NULL, UNDEFINED)
+
+// let string = "";
+// let boolean = true;
+// let number = 1;
+// let nullValue = null;
+// let noValue;
+
+// console.log(Number(boolean));
+
+//ARITHMETIC OPERATORS
+
+// +
+// -
+// /
+// %
+
+//Comparison Operators
+
+// ==
+// ===
+// <
+// >
+// !==
+// <=
+// >=
+
+// let variable = 'John'
+// let newVariable = 'John'
+
+// console.log(variable === newVariable);
+
+//Logical operators
+
+// && and
+// || or
+// !  not
+
+// let variable = 'John'
+// let newVariable = 'John'
+
+// console.log(variable !== 'Johnn');
+
+//CONDITIONS
+
+// let x = "";
+
+// if (typeof(x)==='string') {
+//   console.log("The value is correct");
+// } else {
+//   console.log("The value is incorrect");
+// }
+
+// if (condition) {
+
+// } else if(){
+
+// }else if(){
+
+// }else if{
+
+// }else{
+
+// }
+
+// let variable = 0;
+
+// if (typeof variable === "string") {
+//   console.log("This is a string");
+// } else if (typeof variable === "number") {
+//   console.log("This is a number");
+// } else if (typeof variable === "boolean") {
+//   console.log("This is a boolean");
+// } else {
+//   console.log("no value");
+// }
+
+//SWITCH
+
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
+// const key = true;
+
+// switch (key) {
+//   case "value":
+//     console.log("This is the right answer");
+//     break;
+//   case 8:
+//     console.log("The val is a number");
+//     break;
+//   default:
+//     console.log("None of the conditions were met");
+// }
+// let x = "string";
+
+// typeof x === "string" ? console.log("the value is string") : console.log("the value is not string");
+
+// Conditional Operator
+
+// Create two boolean variables called `isSunnyDay` and `isHomeworkCompleted`.
+// Assign them a value. Use the Ternary Operator in order to print `Jesse can go out to play`,
+// if both variables are true. If one of them or both are false print `Jesse stays home`.
+
+// let isSunnyDay = true
+
+// let isHomeworkCompleted = true
+
+// // if (isSunnyDay && isHomeworkCompleted) {
+// //   console.log('Jesse can go out to play');
+// // }
+// // else{
+// //   console.log('Jesse stays home');
+// // }
+
+// isSunnyDay && isHomeworkCompleted ? console.log('Jesse can go out to play'):console.log('Jesse can go out to play');
+
+// If/else statement
+
+// Given the variables `name` and `surname`, create an `if else` statement to print the full name.
+// The full name must be printed only if `name` and `surname` are `truthy`,
+// otherwise print the message: `Full name is invalid`.
+
+// let name;
+// let surname = "Rossi";
+// let fullname = `${name} ${surname}`; //alt + 96
+
+// if (name && surname) {
+//   console.log(fullname);
+// } else {
+//   console.log("Full name is invalid");
+// }
+
+// Create a variable called `password`.
+// If the password length is between 4 and 6, print
+// the message `Password length is correct`, otherwise print the message `Password not valid`.
+
+// let password = 'pass';
+
+// if(password.length >= 4 && password.length <= 6){
+//   console.log(`Password length is correct ${password.length}`);
+// }else{
+//   console.log(`Password not valid ${password.length}`);
+// }
+
+// let name =['m','a','r','i','o']
+
+// for (let index = 0; index < name.length; index++) {
+//   console.log(index);
+// }
+
+// for (const iterator of name) {
+//   console.log(iterator);
+// }
+
+// for (const key in name) {
+//   console.log(name[key]);
+// }
+
+// Print in the console all the even numbers from 0 to 20
+
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
